fc52aac997a101cce46fea9525a3ef37839177e1
Viet Bach <vbachh.bui1611@gmail.com>
2023-11-23 18:56:53 +0700

    Update Caesar.py

--- project/Caesar.py
+++ project/Caesar.py
@@ -2,7 +2,7 @@ def caesar_encrypt(text, shift):
     encrypted_text = ""
     for char in text:
         ascii_val = ord(char)
-        new_ascii_val = (ascii_val + shift) % 256
+        new_ascii_val = (ascii_val + shift) % 99999
         encrypted_text += chr(new_ascii_val)
     return encrypted_text
 
@@ -10,13 +10,13 @@ def caesar_decrypt(encrypted_text, shift):
     decrypted_text = ""
     for char in encrypted_text:
         ascii_val = ord(char)
-        new_ascii_val = (ascii_val - shift) % 256
+        new_ascii_val = (ascii_val - shift) % 99999
         decrypted_text += chr(new_ascii_val)
     return decrypted_text
 
 # Test the functions
 text = "Hello, World! @@@"
-shift = 1314
+shift = 82000
 
 print(f"Plaintext: {text}")
 
