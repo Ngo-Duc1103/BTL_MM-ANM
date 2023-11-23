eb9574caa04539f81ebf8c68975bb6c1d7693cf7
Viet Bach <vbachh.bui1611@gmail.com>
2023-11-23 18:56:13 +0700

    Update Multiplicative.py

--- project/Multiplicative.py
+++ project/Multiplicative.py
@@ -1,31 +1,26 @@
-def multiplicative_inverse(a, m):
-    for i in range(1, m):
-        if (a * i) % m == 1:
-            return i
-    return -1
-
 def multiplicative_encrypt(text, key):
     encrypted_text = ""
     for char in text:
         ascii_val = ord(char)
-        new_ascii_val = (ascii_val * key) % 256
+        new_ascii_val = (ascii_val * key) % 99999
         encrypted_text += chr(new_ascii_val)
     return encrypted_text
 
 def multiplicative_decrypt(encrypted_text, key):
     decrypted_text = ""
-    inverse_key = multiplicative_inverse(key, 256)
-    if inverse_key == -1:
-        return "Error: Key is not invertible"
     for char in encrypted_text:
         ascii_val = ord(char)
-        new_ascii_val = (ascii_val * inverse_key) % 256
+        # Find multiplicative inverse
+        multiplicative_inverse = pow(key, -1, 99999)
+        new_ascii_val = (ascii_val * multiplicative_inverse) % 99999
         decrypted_text += chr(new_ascii_val)
     return decrypted_text
 
 # Test the functions
-text = "Hello, World!"
-key = 7
+text = "Hello, World! @@@"
+key = 82000
+
+print(f"Plaintext: {text}")
 
 encrypted = multiplicative_encrypt(text, key)
 print(f"Encrypted: {encrypted}")
